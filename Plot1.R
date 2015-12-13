5191a0e43f147c8566ffcb07d70bd3b545ae909f
jamiefo <jamesforesto@gmail.com>
2015-12-13 08:51:37 -0600

    Graphics edit

--- Plot1.R
+++ Plot1.R
@@ -1,14 +1,16 @@
-# Set our working direction - (remember to use / slash in "quotes")
+# Set our working direction 
 setwd("C:/Users/uszlj8j/Desktop/Learning R/Data Science CourseRA/R_Graphics")
 getwd()
 mydata <- read.table("C:/Users/uszlj8j/Desktop/Learning R/Data Science CourseRA/R_Graphics/household_power_consumption.txt",header = TRUE, sep = ";", stringsAsFactors=FALSE, dec=".")
 View(mydata)
 
-###################  PLOT-1 ##############################
 ####Subset between 2 character Dates
 mydata2 <- mydata[mydata$Date %in% c("1/2/2007","2/2/2007") ,]
 mydata2$Global_active_power <- as.numeric(mydata2$Global_active_power)
-head(mydata2)  
+head(mydata2) 
+
+###################  PLOT-1 ##############################
+
 
 png("plot1.png", width=480, height=480)
 plot1 <- hist(mydata2$Global_active_power,
--- Plot2.R
+++ Plot2.R
@@ -1,8 +1,13 @@
-# Set our working direction - (remember to use / slash in "quotes")
+# Set our working direction 
 setwd("C:/Users/uszlj8j/Desktop/Learning R/Data Science CourseRA/R_Graphics")
 getwd()
 mydata <- read.table("C:/Users/uszlj8j/Desktop/Learning R/Data Science CourseRA/R_Graphics/household_power_consumption.txt",header = TRUE, sep = ";", stringsAsFactors=FALSE, dec=".")
 View(mydata)
+
+####Subset between 2 character Dates
+mydata2 <- mydata[mydata$Date %in% c("1/2/2007","2/2/2007") ,]
+mydata2$Global_active_power <- as.numeric(mydata2$Global_active_power)
+head(mydata2)
 
 ###################  PLOT-2 ##############################
 
